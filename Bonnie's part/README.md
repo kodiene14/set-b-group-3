2ce233fe7cf2cd5c05c4c406fe7c879999f8e4fc
kodiene14 <71950592+kodiene14@users.noreply.github.com>
2021-03-08 23:09:47 -0800

    kodi's commit

--- Bonnie's part/README.md
+++
@@ -1,1 +0,0 @@
-# set-b-group-3
--- Bonnie's part/wronganswer.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".heading").innerHTML += HeadingUI("#974A4A", "Sorry wrong answer", "36");
-
-document.querySelector(".button").innerHTML += TryAgainUI();
-
-function options(){
-    window.location.href = "whylitter.html";
-}
